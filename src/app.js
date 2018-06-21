a4cee8c87fd3a7355258adc056ed9bd8b6a0ff23
Nikita Nafranets <eddimensi@gmail.com>
2018-06-22 00:33:55 +0300

    first version

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import Telegraf from 'telegraf'
+import uuidv4 from 'uuid/v4'
+import getURL from './getURL'
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
 
-console.log('STARTING')
-bot.startPolling(1)
+const createAnswer = (query, link) => ({
+  type: 'article',
+  title: query,
+  id: uuidv4(),
+  input_message_content: {
+    message_text: link,
+    disable_web_page_preview: true
+  }
+})
+
+bot.on('inline_query', async ctx => {
+  console.log(ctx.inlineQuery)
+  if (!ctx.inlineQuery.query) return
+  const link = await getURL(ctx.inlineQuery.query)
+  ctx.answerInlineQuery([
+    createAnswer(ctx.inlineQuery.query, link)
+  ])
+})
+
+bot.on('callback_query', (ctx) => {
+  console.log('callback', ctx)
+})
+
+bot.startPolling()
