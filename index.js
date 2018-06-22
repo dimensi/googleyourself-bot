1a1a635779dbff0cf4624736ecde22c612703fc4
Nikita Nafranets <eddimensi@gmail.com>
2018-06-22 23:17:07 +0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 const dotenv = require('dotenv')
 dotenv.load()
-const envConfig = dotenv.parse(fs.readFileSync('.env.local'))
-for (var k in envConfig) {
-  process.env[k] = envConfig[k]
-}
+try {
+  const envConfig = dotenv.parse(fs.readFileSync('.env.local'))
+  for (var k in envConfig) {
+    process.env[k] = envConfig[k]
+  }
+} catch (err) {}
 require = require('esm')(module) //eslint-disable-line
 module.exports = require('./src/app.js')
