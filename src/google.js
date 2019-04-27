dfcf5c9737ec6432f669b7cffff68a799197b0da
Nikita Nafranets <eddimensi@gmail.com>
2019-04-27 15:27:29 +0300

    update build

--- src/google.js
+++ src/google.js
@@ -1,6 +1,16 @@
 import { createBot } from './createBot'
 import generateAnswers from './yourselfSearch'
 
-const bot = createBot('google', process.env.GOOGLE_BOT_TOKEN, [generateAnswers])
-bot.telegram.setWebhook(process.env.WEBHOOK_PATH)
-bot.startWebhook(process.env.WEBHOOK_PATH)
+async function init () {
+  const bot = createBot('google', process.env.GOOGLE_BOT_TOKEN, [generateAnswers])
+  const success = await bot.telegram.setWebhook(process.env.ALIAS + process.env.WEBHOOK_PATH)
+  if (success) {
+    console.log('SUCCESS SAVE WEBHOOK', success)
+  } else {
+    console.log('FAIL', success)
+  }
+  bot.startWebhook(process.env.WEBHOOK_PATH)
+  console.log('START BOT ON PATH', process.env.WEBHOOK_PATH)
+}
+
+init()
