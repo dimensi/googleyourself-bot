786bd1b1f7d0abc58f16a7509550a85b7f1322f8
Nikita Nafranets <eddimensi@gmail.com>
2019-04-27 15:01:28 +0300

    update dependencies

--- src/google.js
+++ src/google.js
@@ -2,4 +2,5 @@ import { createBot } from './createBot'
 import generateAnswers from './yourselfSearch'
 
 const bot = createBot('google', process.env.GOOGLE_BOT_TOKEN, [generateAnswers])
+bot.telegram.setWebhook(process.env.WEBHOOK_PATH)
 bot.startWebhook(process.env.WEBHOOK_PATH)
